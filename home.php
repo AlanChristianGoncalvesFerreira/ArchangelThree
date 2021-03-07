d4d6296613fac8a56f25268cce38f23d3b42971e
Alan Christian Gonçalves Ferreira <alanchristiangoncalvesferreira@hotmail.com>
2021-03-07 14:23:58 -0300

    Add files via upload

--- home.php
+++ home.php
@@ -1,1 +1,42 @@
-<p>HOME BEM VINDO!</p> 
+<div id="cHome">
+ <figure>
+     <img src="images/logoone.png" alt="Alan Christian">
+ </figure> 
+ <article>
+     <h2>Sobre mim </h2>
+     <p>Alan Christian Gonçalves Ferreira , designer gráfico e desenvolvedor 
+front end , desde 2010 . Já são 11 anos de carreira , trabalhando para 
+pequenas , médias e grandes empresas .</p>
+ </article>
+
+
+</div>    
+
+<style>
+#cHome{}
+#cHome figure{display:contents; float:left;}
+#cHome figure img{     
+    width: 50%;
+    height: 114%;
+        
+
+}
+#cHome article{   
+    width: 46%;
+    color: black;
+    font-size: 1.2em;
+    float:right;
+    margin-top: 20%;
+}
+@media only screen and (max-width: 824px) { 
+#cHome figure img {
+    width: 97%;
+    height: 90%;
+}
+#cHome article {
+  float:none;
+
+}
+
+}
+</style>
