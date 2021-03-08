af519a7433892d0c5f737bf4390a237490406556
Alan Christian Gonçalves Ferreira <alanchristiangoncalvesferreira@hotmail.com>
2021-03-07 23:08:31 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type="text/javascript" src="script.js"></script>
     <link rel="stylesheet" href="style.css ">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-77245717-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-77245717-1');
+</script>
 
     <title>Alan Christian / Archangel Three</title>
 </head>
