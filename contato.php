901f2be002fb4e7a711cabb43a2b617f966db9c1
Alan Christian Gonçalves Ferreira <alanchristiangoncalvesferreira@hotmail.com>
2021-03-15 13:01:45 -0300

    Add files via upload

--- contato.php
+++ contato.php
@@ -2,12 +2,14 @@
 
 <div id="cServicos">
 
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScT1gxkBqQJTzlxhO1pJE2UPjtTJVJQ4i-ZD27DUbixF5bkMw/viewform?embedded=true" width="440" height="636" frameborder="0" marginheight="0" marginwidth="0">Carregando…</iframe>
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScT1gxkBqQJTzlxhO1pJE2UPjtTJVJQ4i-ZD27DUbixF5bkMw/viewform?embedded=true" width="640" height="636" frameborder="0" marginheight="0" marginwidth="0">Carregando…</iframe>
 
 </div>
 
 <style>
  
-
+#cServicos{width: 100%;
+    height: 100%;
+    display: flex;}
 
 </style>
