1d2e0adf2002285808a12783c7c9a86465b0305f
Alan Christian Gonçalves Ferreira <alanchristiangoncalvesferreira@hotmail.com>
2021-03-15 12:52:54 -0300

    Add files via upload

--- contato.php
+++ contato.php
@@ -2,52 +2,12 @@
 
 <div id="cServicos">
 
-
-</div>
-<div id="cForm">
-<form action="index.php" method="post" onsubmit="return valida_form(this)">
-<fieldset>
-    <legend>Contato</legend>
-<label for="nome"> Nome :</label></br>
-<input type="text" id="nome" name="nome" value="digite aqui"></br>
-<label for="email"> Email : </label></br>
-<input type="email" name="email" id="email" ></br>
-<textarea name="message" rows="10" cols="30" id="message">
-
-</textarea></br>
-<input type="submit" value="Submit" class="button">
-
-</fieldset>
-</form>
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScT1gxkBqQJTzlxhO1pJE2UPjtTJVJQ4i-ZD27DUbixF5bkMw/viewform?embedded=true" width="440" height="636" frameborder="0" marginheight="0" marginwidth="0">Carregando…</iframe>
 
 </div>
 
 <style>
-fieldset{width: 47%;
-        height: 75%;
-        margin: 0 auto;
-    color:black;  }
-form input{ width:50%;
-            min-width: 198px;
-            height:40px;
-            margin:10px;}
-form textarea{width:70%;
-            min-width:198px;
-             height: 63%;
-            margin:10px;  }            
-
-.button { width:30%;    
-          height:40px;
-         color:black;
-         background-color: #f14f11;
-    font-size: 1.2em;
-    border-radius: 10px;  }
-  
-    @media only screen and (max-width: 824px) {
-
-
-
-     }   
+ 
 
 
 </style>
